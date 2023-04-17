b88cd81d7f9a183bb79b56559d38da3f1d8fd804
Maryin-c <3317701822@qq.com>
2023-04-18 00:51:10 +0800

    [feature]:增加用户登录接口

--- dkhtn_django/user/views.py
+++ dkhtn_django/user/views.py
@@ -1,7 +1,11 @@
 # from django.contrib.auth.models import User
 from django.http import JsonResponse
-from .rabbit.RabbitMQ import rabbit_mq
 from django.conf import settings
+from django.contrib import auth
+
+from .rabbit.RabbitMQ import rabbit_mq
+from .models import User
+from .wrappers import wrapper_set_login
 
 
 def email_send(request):
@@ -33,3 +37,33 @@ def rsa_get(request):
         "data": settings.RSA_PUBLIC_KEY,
     }
     return JsonResponse(response)
+
+
+# 登录接口，暂时不允许多点登录，多点登录涉及同步问题
+@wrapper_set_login
+def login(request):
+    try:
+        username = request.POST.get('uname')
+        password = request.POST.get('password')
+        user = auth.authenticate(username=username, password=password)
+        # 登录信息登记
+        request.userinfo = User(user).userinfo()
+        if user is None:
+            response = {
+                "code": 1,
+                "message": "用户名或密码错误",
+            }
+            return JsonResponse(response)
+        else:
+            response = {
+                "code": 0,
+                "message": "登录成功",
+            }
+            return JsonResponse(response)
+    except Exception as e:
+        raise e
+        # response = {
+        #     "code": 114514,
+        #     "message": e.__str__(),
+        # }
+        # return JsonResponse(response)
